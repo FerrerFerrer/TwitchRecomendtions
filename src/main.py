9ced92fc14d7b7b8b0df3d66bdbddbce5403176d
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-10-26 21:05:23 -0500

    Global path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 #Main program - User Interface
+
+#Imports
 import pickle #converts python objects into string representation and viceversa
 
-
-ls_channel = []
+#Global variables
+ls_channel = [] #List to saves the user fav channels
+PATH = "saves\{}.txt" #Static path name to save options for later
 
 def printOptions():
     pass
@@ -14,9 +17,10 @@ def doAction(comand):
     pass
 
 def main():
+    # static_sim = "saves\{}.txt"
     # ls_channel.append("Prueba")
     # t = 0
-    # with open("saves\prueba.txt", "wb") as temp_path:
+    # with open(static_sim.format("prueba"), "wb") as temp_path:
     #     pickle.dump(ls_channel, temp_path)
     #
     # with open("saves\prueba.txt", "rb") as temp_path:
