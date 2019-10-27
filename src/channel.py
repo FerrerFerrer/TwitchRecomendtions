f547cf72990367e8e4dc84103f6e49e42c1f2afc
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-10-26 20:06:55 -0500

    added classes

---
+++ src/channel.py
@@ -0,0 +1,9 @@
+class Channel:
+    def __init__(self, id, ponderation = 0):
+        self.id = id
+        self. ponderation = ponderation
+
+    def addPonderation(num):
+        self.ponderation += num
+
+    
--- src/main.py
+++ src/main.py
@@ -1,2 +1,22 @@
+#Main program - User Interface
+
+#Add a channel
+def addFollower(channel):
+    pass
+
+def removeFollower(channel):
+    pass
+
+def saveFollowers(path):
+    pass
+
+def retriveFollowers(path):
+    pass
+
+def getFollowersFromChannel(channel):
+    pass
+
+def 
+
 if __name__ == '__main__':
     main()
