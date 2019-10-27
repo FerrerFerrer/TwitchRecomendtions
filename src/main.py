64875a1ca5ec909854baec192b5a0d119bf1d617
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-10-26 20:10:20 -0500

    added comments

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 #Main program - User Interface
 
-#Add a channel
+#Add a channel locally
 def addFollower(channel):
     pass
 
+#Reomve a channel locally
 def removeFollower(channel):
     pass
 
+#Save list of chanels to a file
 def saveFollowers(path):
     pass
 
+#Retrive a lost of Channeles from a file
 def retriveFollowers(path):
     pass
 
+#Connect to api to retrive followees from a spesific channel
 def getFollowersFromChannel(channel):
     pass
 
-def 
+#It adds to the ponderation bases on how many local channels follow the same one
+def ponderByUnionOfChannels(channels):
+    pass
 
 if __name__ == '__main__':
     main()
