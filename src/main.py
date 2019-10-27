248f88f2d82bd08d0bfec65b63d2b51566a034eb
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-10-26 20:57:50 -0500

    Sample on how to save variables to txt

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 #Main program - User Interface
+import pickle #converts python objects into string representation and viceversa
+
 
 ls_channel = []
 
@@ -12,6 +14,16 @@ def doAction(comand):
     pass
 
 def main():
+    # ls_channel.append("Prueba")
+    # t = 0
+    # with open("saves\prueba.txt", "wb") as temp_path:
+    #     pickle.dump(ls_channel, temp_path)
+    #
+    # with open("saves\prueba.txt", "rb") as temp_path:
+    #     t = pickle.load(temp_path)
+    #
+    # print(t)
+
     pass
 
 if __name__ == '__main__':
