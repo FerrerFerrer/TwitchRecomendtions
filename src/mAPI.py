7073f6c4ed8cf204cfde14ad8cfac5cd96e70d1f
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-10 12:35:52 -0600

    added

---
+++ src/mAPI.py
@@ -0,0 +1,21 @@
+from abs.absAPI import AbstactClassApi
+
+class mockAPI(AbstactClassApi):
+        def __init__(self, *args):
+            super().__init__(*args)
+
+        def get_userid(self, name):
+            return "4732"
+
+        #Who he follows - should work both with id's and name
+        def get_followers(self, name_or_id):
+            l = ["a", "b", "c", "d"]
+            return l
+
+        def get_name(self, id):
+            return "StreamerGamer"
+
+        #Who follows him - should work both with id's and name
+        def get_followe(self, name_or_id):
+            l = ["z", "x", "l", "p", "i"]
+            return l
--- src/mBolt.py
+++ src/mBolt.py
@@ -1,16 +1,27 @@
 from abs.absBolt import AbstactBolt
+from classes import channel
 
 class mockBolt(AbstactBolt):
-        def __init__(self):
+        def __init__(self, api, bd):
             super().__init__()
+            self.api = api
+            self.bd = bd
 
-        def addChanel(self, channel):
-            print("Adding...", channel)
+        def addChanel(self, name):
+            self.ls_channel.append(name)
+            print("Adding...", name)
 
-        def removeChanel(self, channel):
-            print("Removing...", channel)
+        def removeChanel(self, name):
+            self.ls_channel.remove(name)
+            print("Removing...", name)
 
-        def blockChanel(self, channel):
+        def blockChanel(self, name):
+            for x in self.ls_channel:
+                t = x.name
+                if t is name:
+                    x.block(True)
+                    return
+
             print("Blocking...", channel)
 
         def calculate(self):
@@ -18,3 +29,6 @@ class mockBolt(AbstactBolt):
 
         def printRecomendations(self):
             print("This are your recomendations!")
+
+        def get_index_in_list(self, name):
+            for
