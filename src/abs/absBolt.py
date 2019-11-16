bc67aabc275b841334d900e018004bbfce8ee485
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-15 18:57:34 -0600

    conected bd to bolt

--- src/abs/absBolt.py
+++ src/abs/absBolt.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
-class AbstactBolt(ABC):
+class AbstactBolt(ABC, basedatos):
     def __init__(self):
         super().__init__()
         self.ls_channel = []
+        self.bd = basedatos
 
     @abstractmethod
     def addChanel(self, channel):
@@ -23,4 +24,14 @@ class AbstactBolt(ABC):
 
     @abstractmethod
     def printRecomendations(self):
+        pass
+
+    @abstractmethod
+    def save_session(self):
+        #bd.save_list(self.ls_channel)
+        pass
+
+    @abstractmethod
+    def get_session(self):
+        #self.ls_channel = bd.get_list()
         pass
