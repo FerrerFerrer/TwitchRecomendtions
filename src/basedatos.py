e9593b75125062052878e8a2c351e7c655385181
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-24 16:10:26 -0600

    rth

--- src/basedatos.py
+++ src/basedatos.py
@@ -29,12 +29,11 @@ def crear_tablas():
             CREATE TABLE IF NOT EXISTS usuario(
                 user_id TEXT PRIMARY KEY,
                 user_name TEXT NOT NULL,
-                user_id_followers TEXT NOT NULL,
-                user_id_baneados TEXT NOT NULL,
-                user_id_recomendado TEXT NOT NULL,
-                FOREIGN KEY (user_id_followers) REFERENCES followers (user_id),
-                FOREIGN KEY (user_id_baneados) REFERENCES banned (user_id),
-                FOREIGN KEY (user_id_recomendado) REFERENCES followers (user_id)
+                user_followers_id INTEGER,
+                user_followers_name TEXT,
+                baneados TEXT,
+                user_recomendado TEXT,
+                ponderacion float
             );
         '''
         cursor.execute(query)
@@ -48,84 +47,14 @@ def crear_tablas():
             conexion.close()
             print('Conexion a SQLite cerrada\n')
 
-    #TABLAFOLLOWRES
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado a SQLite')
-
-        query = '''
-            CREATE TABLE IF NOT EXISTS followers(
-                user_id TEXT PRIMARY KEY,
-                user_name_seguido TEXT NOT NULL,
-                ponderacion INTEGER NOT NULL
-            );
-        '''
-        cursor.execute(query)
-        row = cursor.fetchall()
-        print('Tabla creada correctamente', row)
-        cursor.close()
-    except sqlite3.Error as error:
-        print('Error con la conexion', error)
-    finally:
-        if(conexion):
-            conexion.close()
-            print('Conexion a SQLite cerrada\n')
-
-    #TABLABANNED
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado a SQLite')
-
-        query = '''
-            CREATE TABLE IF NOT EXISTS banned(
-                user_id TEXT PRIMARY KEY,
-                user_name_baneado TEXT NOT NULL,
-            );
-        '''
-        cursor.execute(query)
-        row = cursor.fetchall()
-        print('Tabla creada correctamente', row)
-        cursor.close()
-    except sqlite3.Error as error:
-        print('Error con la conexion', error)
-    finally:
-        if(conexion):
-            conexion.close()
-            print('Conexion a SQLite cerrada\n')
-
-    #TablaRECOMENDACIONES
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado a SQLite')
-
-        query = '''
-            CREATE TABLE IF NOT EXISTS recomendaciones(
-                user_id TEXT PRIMARY KEY,
-                user_name_recomendado TEXT NOT NULL,
-            );
-        '''
-        cursor.execute(query)
-        row = cursor.fetchall()
-        print('Tabla creada correctamente', row)
-        cursor.close()
-    except sqlite3.Error as error:
-        print('Error con la conexion', error)
-    finally:
-        if(conexion):
-            conexion.close()
-            print('Conexion a SQLite cerrada\n')
-
-def agregar_usuario(id_usuario,nombre_usuaio,id_usuario_seguido,baneados):
+def agregar_usuario(id_usuario,nombre_usuaio,id_usuario_seguido,baneados,nombre_usuario_seguido,ponderacion):
     try:
         conexion = sqlite3.connect('api_twitch.db')
         cursor = conexion.cursor()
         print('Conectado')
 
-        query = """INSERT INTO usuario VALUES 
-                ('{}', '{}', '{}', '{}')""".format(id_usuario, nombre_usuaio, id_usuario_seguido, baneados)
+        query = """INSERT INTO usuario VALUES
+                ({}, '{}', '{}', '{}','{}',{})""".format(id_usuario, nombre_usuaio, id_usuario_seguido, nombre_usuario_seguido, baneados, recomendaciones, ponderacion)
         resultado = cursor.execute(query)
         conexion.commit()
         print('Valor Insertado Correctamente', resultado)
@@ -138,94 +67,35 @@ def agregar_usuario(id_usuario,nombre_usuaio,id_usuario_seguido,baneados):
         if(conexion):
             conexion.close()
 
-def agregar_followers(id_usuario,nombre_usuario,ponderacion):
+def mostrar_usuario():
     try:
         conexion = sqlite3.connect('api_twitch.db')
         cursor = conexion.cursor()
         print('Conectado')
 
-        query = """INSERT INTO followers VALUES 
-                ('{}', '{}','{}')""".format(id_usuario, nombre_usuario, ponderacion)
+        query = """SELECT * FROM usuario"""
         resultado = cursor.execute(query)
         conexion.commit()
-        print('Valor Insertado Correctamente', resultado)
-        cursor.close()
-
-    except sqlite3.Error as error:
-        print('Error con la conexion',error)
-
-    finally:
-        if(conexion):
-            conexion.close()
-
-def agregar_baneados(id_usuario, nombre_usuario):
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado')
-
-        query = """INSERT INTO banned VALUES 
-                ('{}', '{}')""".format(id_usuario, nombre_usuario)
-        resultado = cursor.execute(query)
-        conexion.commit()
-        print('Valor Insertado Correctamente', resultado)
-        cursor.close()
-
-    except sqlite3.Error as error:
-        print('Error con la conexion',error)
-
-    finally:
-        if(conexion):
-            conexion.close()
-
-def agregar_recomendaciones(id_usuario,nombre_usuario):
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado')
-
-        query = """INSERT INTO recomendaciones VALUES 
-                ({}, '{}')""".format(id_usuario, nombre_usuario)
-        resultado = cursor.execute(query)
-        conexion.commit()
-        print('Valor Insertado Correctamente', resultado)
-        cursor.close()
-    except sqlite3.Error as error:
-        print('Error con la conexion',error)
-    finally:
-        if(conexion):
-            conexion.close()
-
-def ver_todos_usuarios():
-    try:
-        conexion = sqlite3.connect('api_twitch.db')
-        cursor = conexion.cursor()
-        print('Conectado')
-
-        query = 'SELECT usu.id_usuario,usu.user_name,fol.user_name_seguido,ban.user_name_baneado,rec.user_name recomendado  FROM usuario usu JOIN follewers fol, banned ban, recomendaciones rec ON fol.user_id = usu.user_id_followers or ban.user_id = usu.user_id_baneados or rec.user_id = usu.user_id_recomendado;'
-        cursor.execute(query)
         rows = cursor.fetchall()
-        print('Total de registros: ', len(rows))
-
-        print('------------Registros-------------')
-
         for row in rows:
-            print('Id_usuario: {}\nNombre_Usuario: {}\nNombre_Usuarios_Seguidos: {}\nNombre_Usuarios_baneados: {}\nNombre_Usuario_Recomendado: {}'.format(*row))
-        
+            print(row)
         cursor.close()
+
     except sqlite3.Error as error:
         print('Error con la conexion',error)
+
     finally:
         if(conexion):
             conexion.close()
 
-def eliminar_usuario(Id_usuario):
+
+def eliminar_usuario(nombre_usuario):
     try:
         conexion = sqlite3.connect('api_twitch.db')
         cursor = conexion.cursor()
         print('Conectado')
 
-        query = "DELETE FROM usuario WHERE user_id = {}".format(Id_usuario)
+        query = "DELETE FROM usuario WHERE user_name = '{}'".format(nombre_usuario)
         resultado = cursor.execute(query)
         conexion.commit()
         print('Valor Eliminado Correctamente', resultado)
