a73d8408aeaa5a8988d3fbad3f8d346f5ede7d89
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-26 17:19:13 -0600

    fixed import

--- testerMaster.py
+++ testerMaster.py
@@ -7,7 +7,7 @@ sys.path.append('src/imp/BoltFuncional.py')
 
 from src.main_test import TestMain
 from test.integracion import TestIntegration
-from src.imp.BoltFuncional import TestBolt
+from src.imp.BoltFuncional_test import TestBolt
 from src.classes.channel_test import TestChannel
 
 #Unitarios
