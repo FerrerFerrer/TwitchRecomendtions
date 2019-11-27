f7252500fde0556f2e89363a197c4efb0e82bdfa
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-26 19:56:16 -0600

    del

--- src/imp/BoltFuncional_test.py
+++ src/imp/BoltFuncional_test.py
@@ -13,6 +13,7 @@ from unittest.mock import MagicMock
 
 
 class TestBolt(unittest.TestCase):
+
     def setUp(self):
         #Channels
         ch1 = Channel(12)
@@ -24,6 +25,9 @@ class TestBolt(unittest.TestCase):
         ch7 = Channel(17, 1, name = "Chuy")
         ch8 = Channel(17, 7, name = "Lupe")
         self.channels = [ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8]
+        self.apireturn = [["Carlos", 1], ["Roberto", 2], ["Rodrigo", 3], ["Jesus", 4],
+        ["Ricardo", 5], ["Chuy", 6], ["Lupe", 7], ["Ricardo", 8]]
+
 
         #Bolt
         #Need to patch..?
@@ -32,22 +36,26 @@ class TestBolt(unittest.TestCase):
 
 
     def testAddCHannel(self):
-        apireturn = [["Carlos", 1], ["Roberto", 2], ["Rodrigo", 3], ["Jesus", 4],
-        ["Ricardo", 5], ["Chuy", 6], ["Lupe", 7], ["Ricardo", 8]]
 
-        for r in range(len(self.channels)):
+        for r in range(len(self.apireturn)):
             with self.subTest(r=r):
-                self.testingBolt.api.get_userid = MagicMock(return_value = apireturn[r][1])
+                self.testingBolt.api.get_userid = MagicMock(return_value = self.apireturn[r][1])
 
-                self.testingBolt.addChanel(apireturn[r][0])
+                self.testingBolt.addChanel(self.apireturn[r][0])
 
                 for l in range(0,r):
                     chan = self.testingBolt.ls_channel[l]
                     name = chan.name
                     id = chan.id
 
-                    self.assertEqual(name, apireturn[l][0])
-                    self.assertEqual(id, apireturn[l][1])
+                    self.assertEqual(name, self.apireturn[l][0])
+                    self.assertEqual(id, self.apireturn[l][1])
+        return self.testingBolt
+
+    def testRemoveChanel(self):
+        self.testingBolt.ls_channel = self.channels[2:]
+        
+
 
 
 if __name__ == '__main__':
