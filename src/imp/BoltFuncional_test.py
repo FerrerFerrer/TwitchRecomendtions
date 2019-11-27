f37d372328a01ac4afcfec6327555483d6bdc7d4
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2019-11-26 19:39:16 -0600

    bolt addchannel tested

--- src/imp/BoltFuncional_test.py
+++ src/imp/BoltFuncional_test.py
@@ -1,5 +1,6 @@
 from BoltFunctional import Bolt
 from channel import Channel
+from APIfuncional import apiTwitch
 
 import unittest
 from unittest.mock import Mock
@@ -26,14 +27,28 @@ class TestBolt(unittest.TestCase):
 
         #Bolt
         #Need to patch..?
-        api = MagicMock(return_value = "yes")
+        api = apiTwitch()
         self.testingBolt = Bolt(api, "bd")
 
+
     def testAddCHannel(self):
+        apireturn = [["Carlos", 1], ["Roberto", 2], ["Rodrigo", 3], ["Jesus", 4],
+        ["Ricardo", 5], ["Chuy", 6], ["Lupe", 7], ["Ricardo", 8]]
+
         for r in range(len(self.channels)):
             with self.subTest(r=r):
-                temp_chan = self.testingBolt.addChanel(self.channels[r])
-                self.assertEqual(self.testingBolt.ls_recomendated, self.channels[:r])
+                self.testingBolt.api.get_userid = MagicMock(return_value = apireturn[r][1])
+
+                self.testingBolt.addChanel(apireturn[r][0])
+
+                for l in range(0,r):
+                    chan = self.testingBolt.ls_channel[l]
+                    name = chan.name
+                    id = chan.id
+
+                    self.assertEqual(name, apireturn[l][0])
+                    self.assertEqual(id, apireturn[l][1])
+
 
 if __name__ == '__main__':
     unittest.main()
